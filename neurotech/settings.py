f30d117ae0bed0c194737e1f17b7c3b5a1511253
good4hwan729 <65903792+good4hwan729@users.noreply.github.com>
2020-07-22 21:29:18 +0900

    Update settings.py

--- neurotech/settings.py
+++ neurotech/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-eyk5whsu4r(bim*reo5+$qbv7#uqwo1$5nm#l4rkz9pn)*j$r'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
